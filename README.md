77317506311f4473d5133801c3458cf88025aa9a
shang9616 <277909309@qq.com>
2016-04-13 11:00:03 +0800

    add post template

--- README.md
+++ README.md
@@ -3,4 +3,12 @@
 《Discover Meteor》中文版网址：http://zh.discovermeteor.com，可下载为PDF文件，方便阅读。
 
 # Meteor
-Meteor版本:1.3.1
+Meteor版本:1.3.1<br>
+
+## Day2
+1、创建postsList、postItem模板。<br>
+
+## Day 1
+1、使用Github For Mac版本，在本地创建microscope的Repository。<br>
+2、在终端输入：meteor crteate microscope && cd microscope。<br>
+3、修改client目录下的main.html文件。
---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,19 @@
+import {Template} from 'meteor/templating';
+
+var postData = [
+	{
+       title: 'Introducing Telescope',
+       url: 'http://sachagreif.com/introducing-telescope/'
+     },
+     {
+       title: 'Meteor',
+       url: 'http://meteor.com'
+	}, {
+      title: 'The Meteor Book',
+      url: 'http://themeteorbook.com'
+     }
+];
+
+Template.postsList.helpers({
+	posts: postData
+});
